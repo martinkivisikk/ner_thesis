c9e9da2566b1e1d6b6b151311a9ec11753800c3c
martinkivisikk <martin.kivisikk@gmail.com>
2025-03-05 17:29:03 +0200

    Kommentaarid, mõni funktsioon puhtamaks

--- modules/data_processing.py
+++ modules/data_processing.py
@@ -9,7 +9,7 @@ from datasets import Dataset, DatasetDict
 # siis töötle andmed uuesti (from_json=False ja salvesta kasutades save_dataset_to_json)
 
 class DatasetProcessor:
-    # potentsiaalne veakoht, kasuta sort ja tee andmestik ümber
+    # peab jälgima, et all_tags, idx2tag ja mudeli.config.id2label/label2id ja andmestik on kooskõlas
     ALL_TAGS = ['O', 
             'B-EVE', 'I-EVE', 
             'B-GEP', 'I-GEP', 
@@ -31,7 +31,7 @@ class DatasetProcessor:
                     'B-Per', 'I-Per',
                     'B-Prod', 'I-Prod',
                     'B-Unk', 'I-Unk']
-    # Kuna andmestikus on üksikud vead, aga on enam-vähem selge, mida tegelikult mõeldi, siis teeme vastavad parandused.
+    # Kuna andmestikus on üksikud vead, aga on enam-vähem selge, mida tegelikult mõeldi, siis vastavad parandused.
     CORRECTIONS = {
             'B-OrgSpaceAfter': 'B-Org',
             'B_Gep': 'B-Gep',
@@ -69,63 +69,9 @@ class DatasetProcessor:
     
     @classmethod
     def replace_chars(cls, text):
+        # Asendab àùò äüo
         return cls.PATTERN.sub(lambda m: cls.CHAR_MAP[m.group(0)], text)
     
-    # def get_split_as_text(self, split_path:str):
-    #     text = conll_importer.conll_to_text(file=split_path)
-    #     return text
-    
-    # def get_all_splits_as_text(self):
-    #     train_text = self.get_split_as_text(self.dataset_paths['train'])
-    #     test_text = self.get_split_as_text(self.dataset_paths['test'])
-    #     dev_text = self.get_split_as_text(self.dataset_paths['dev'])
-    #     return train_text, test_text, dev_text
-
-    def get_train_data_for_tagger(self):
-        train_texts = self.get_split_as_texts_list(self.dataset_paths['train'])
-        train_tags = self.get_all_tag_lists(train_texts)
-        return train_texts, train_tags
-
-    def get_test_data_for_tagger(self):
-        test_texts = self.get_split_as_texts_list(self.dataset_paths['test'])
-        test_tags = self.get_all_tag_lists(test_texts)
-        return test_texts, test_tags
-
-    def get_split_as_texts_list(self, split_path:str):
-        texts = conll_importer.conll_to_texts_list(file=split_path)
-        for text in texts:
-          text.tag_layer(['morph_analysis'])
-        return texts
-
-    def get_tag_lists(self, text: Text):
-        #Ühe Text objekti lausete märgendid listide listina
-        parsed_sents = []
-        for sent in text.sentences:
-            tags = []
-            
-            for misc in sent.conll_syntax.misc:
-                tag = 'O'
-                if misc and 'NE' in misc:
-                    ne_tag = misc['NE']
-                    if ne_tag in self.KNOWN_TAGS:
-                        tag = ne_tag
-                    elif tags and tags[-1] == 'B-ORG' and ne_tag == 'Org':
-                        tag = 'I-Org'
-                    elif tags and tags[-1] == 'B-PER' and ne_tag == 'Per':
-                        tag = 'I-Per'
-                    else:
-                        tag = self.CORRECTIONS.get(ne_tag, 'O')
-                tags.append(tag.upper())
-
-            parsed_sents.append(tags)
-        return parsed_sents
-
-    def get_all_tag_lists(self, texts):
-        tag_lists = []
-        for text in texts:
-            tag_lists.append(self.get_tag_lists(text))
-        return tag_lists
-
     def preprocess(self, dataset_path: str, use_lemmas=False):
         # Sisend: andmestiku failitee sõnena
         # Väljund: List, mis sisaldab parsitud lauseid
@@ -148,6 +94,8 @@ class DatasetProcessor:
         return parsed_sents
     
     def _get_tag(self, misc, parsed_sent):
+        # Sisendiks siiani parsitud lause, ja failist misc info
+        # Tagastab sõnale vastava märgendi
         if not misc or 'NE' not in misc:
             return 'O'
         ne_tag = misc['NE']
@@ -166,37 +114,47 @@ class DatasetProcessor:
     
     def split_to_token_and_tag(self, sents):
         # Sisend: parsitud laused
-        # Väljund: Sõnastike list
-        # Sõnastik sisaldab kolme elementi: lause ID täisarvuna, märgendite list arvulisel kujul ning sõnade list
-        res = {}
-        for i, sent in enumerate(sents):
-            tags = [self.TAG2IDX[tag] for _, tag in sent]
-            words = [word for word, _ in sent]
-            res[i] = {
-                'id': i,
-                'tags': tags,
-                'tokens': words
-                }
-        return res
+        # Väljund: 
+        return {
+            "id": list(range(len(sents))),
+            "tags": [[self.TAG2IDX[tag] for _, tag in sent] for sent in sents],
+            "tokens": [[word for word, _ in sent] for sent in sents]
+        }
+        # res = {}
+        # for i, sent in enumerate(sents):
+        #     tags = [self.TAG2IDX[tag] for _, tag in sent]
+        #     words = [word for word, _ in sent]
+        #     res[i] = {
+        #         'id': i,
+        #         'tags': tags,
+        #         'tokens': words
+        #         }
+        # return res
     
-    def transform_set(self, data):
-        transformed = {
-            "id": [v["id"] for v in data.values()],
-            "tags": [v["tags"] for v in data.values()],
-            "tokens": [v["tokens"] for v in data.values()]
-            }
-        return Dataset.from_dict(transformed)
+    # def transform_set(self, data):
+        
+    #     transformed = {
+    #         "id": [v["id"] for v in data.values()],
+    #         "tags": [v["tags"] for v in data.values()],
+    #         "tokens": [v["tokens"] for v in data.values()]
+    #         }
+    #     return Dataset.from_dict(transformed)
     
     def process_all(self, use_lemmas=False):
         train_sents = self.preprocess(self.dataset_paths['train'], use_lemmas)
         dev_sents = self.preprocess(self.dataset_paths['dev'], use_lemmas)
         test_sents = self.preprocess(self.dataset_paths['test'], use_lemmas)
         
-        train_ds = self.transform_set(self.split_to_token_and_tag(train_sents))
-        dev_ds = self.transform_set(self.split_to_token_and_tag(dev_sents))
-        test_ds = self.transform_set(self.split_to_token_and_tag(test_sents))
+        return DatasetDict({
+            'train': Dataset.from_dict(self.split_to_token_and_tag(train_sents)),
+            'dev': Dataset.from_dict(self.split_to_token_and_tag(dev_sents)),
+            'test': Dataset.from_dict(self.split_to_token_and_tag(test_sents))
+        })
+        # train_ds = self.transform_set(self.split_to_token_and_tag(train_sents))
+        # dev_ds = self.transform_set(self.split_to_token_and_tag(dev_sents))
+        # test_ds = self.transform_set(self.split_to_token_and_tag(test_sents))
         
-        return DatasetDict({'train': train_ds, 'dev': dev_ds, 'test': test_ds})
+        # return DatasetDict({'train': train_ds, 'dev': dev_ds, 'test': test_ds})
     
     def load_or_process(self, use_lemmas=False):
         return self.load_dataset_from_json() if self.from_json else self.process_all(use_lemmas)
@@ -260,3 +218,51 @@ class DatasetProcessor:
             "dev": dev_stats,
             "total": total_stats
         }
+
+    # Kasutamata funktsioonid (EstNLTK nertaggeri jaoks, aga ei kasuta)
+
+    def get_train_data_for_tagger(self):
+        train_texts = self.get_split_as_texts_list(self.dataset_paths['train'])
+        train_tags = self.get_all_tag_lists(train_texts)
+        return train_texts, train_tags
+
+    def get_test_data_for_tagger(self):
+        test_texts = self.get_split_as_texts_list(self.dataset_paths['test'])
+        test_tags = self.get_all_tag_lists(test_texts)
+        return test_texts, test_tags
+
+    def get_split_as_texts_list(self, split_path:str):
+        texts = conll_importer.conll_to_texts_list(file=split_path)
+        for text in texts:
+          text.tag_layer(['morph_analysis'])
+        return texts
+
+    def get_tag_lists(self, text: Text):
+        #Ühe Text objekti lausete märgendid listide listina
+        parsed_sents = []
+        for sent in text.sentences:
+            tags = []
+            
+            for misc in sent.conll_syntax.misc:
+                tag = 'O'
+                if misc and 'NE' in misc:
+                    ne_tag = misc['NE']
+                    if ne_tag in self.KNOWN_TAGS:
+                        tag = ne_tag
+                    elif tags and tags[-1] == 'B-ORG' and ne_tag == 'Org':
+                        tag = 'I-Org'
+                    elif tags and tags[-1] == 'B-PER' and ne_tag == 'Per':
+                        tag = 'I-Per'
+                    else:
+                        tag = self.CORRECTIONS.get(ne_tag, 'O')
+                tags.append(tag.upper())
+
+            parsed_sents.append(tags)
+        return parsed_sents
+
+    def get_all_tag_lists(self, texts):
+        tag_lists = []
+        for text in texts:
+            tag_lists.append(self.get_tag_lists(text))
+        return tag_lists
+    
